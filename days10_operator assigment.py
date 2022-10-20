6f33ae230ee77c82df5a1d9c5e12188ca9f92ee4
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-20 04:49:50 -0700

    Add files via upload

---
+++ days10_operator assigment.py
@@ -0,0 +1,23 @@
+#operator penugasan (assigment)
+#merupakan operator penggabugan antara operator aritmatika dan sama dengan (=)
+
+a = 5 #adalah assigment
+print("nilai a = ",a)
+
+a += 3 #artinya a = a(5) + 3
+print (a)
+
+a -= 2 #artinya a = a (8) - 2
+#kenapa 8 karna nilai variabel a = 5 ditambahkan 5 = 8
+print(a)
+#dan selama variabel yang digunakan adalah a maka ia akan terus melakukan assigment
+
+#contoh
+apel = 4
+apel += 6
+apel -= 1
+apel /= 3
+print ( apel )
+
+#artinya 4 + 6 - 1 / 3
+
